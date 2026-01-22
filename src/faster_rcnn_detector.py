dfd5e6f5d6701059958ec573e0eefc5ecb28ea1f
nesterovich-ps <mazafaka2026@gmail.com>
2026-01-22 19:31:03 +0600

    add faster_rcnn_detector.py

---
+++ src/faster_rcnn_detector.py
@@ -0,0 +1,35 @@
+import torch
+import torchvision
+from torchvision.transforms import functional as F
+from .base_detector import BaseDetector
+
+
+class FasterRCNNDetector(BaseDetector):
+    def __init__(self):
+        target_model = "faster_rcnn"
+        model_name = "fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
+        person_class_id = 1
+        super().__init__(target_model, model_name, person_class_id)
+
+    def _load_model(self):
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
+        state_dict = torch.load(self.model_path, map_location=self.device)
+        model.load_state_dict(state_dict)
+        return model
+
+    @torch.no_grad()
+    def predict(self, frame):
+        image = F.to_tensor(frame).to(self.device)
+        output = self.model([image])[0]
+
+        result = []
+        for box, label, score in zip(
+                output["boxes"],
+                output["labels"],
+                output["scores"]
+        ):
+            if label.item() == self.PERSON_CLASS_ID and score >= self.hyperparams["conf_threshold"]:
+                x1, y1, x2, y2 = map(int, box.tolist())
+                result.append((x1, y1, x2, y2, float(score)))
+
+        return result
