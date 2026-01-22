621d2b54bb73a360bb0bae72c990e300ebad3ebd
nesterovich-ps <mazafaka2026@gmail.com>
2026-01-22 19:32:28 +0600

    fix load model

--- src/faster_rcnn_detector.py
+++ src/faster_rcnn_detector.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 from torchvision.transforms import functional as F
@@ -12,9 +13,17 @@ class FasterRCNNDetector(BaseDetector):
         super().__init__(target_model, model_name, person_class_id)
 
     def _load_model(self):
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
-        state_dict = torch.load(self.model_path, map_location=self.device)
-        model.load_state_dict(state_dict)
+
+        os.makedirs(os.path.dirname(self.model_path), exist_ok=True)
+
+        if not os.path.exists(self.model_path):
+            model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+            torch.save(model.state_dict(), self.model_path)
+        else:
+            model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None)
+            state_dict = torch.load(self.model_path, map_location=self.device)
+            model.load_state_dict(state_dict)
+
         return model
 
     @torch.no_grad()
